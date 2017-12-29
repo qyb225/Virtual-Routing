3a3bed36143ebf975a01bedce52875778b852c4b
qyb225 <qyb225@gmail.com>
2017-12-29 11:08:24 +0800

    Refinement steps & function encapsulation

--- src/sender_client.h
+++ src/sender_client.h
@@ -16,9 +16,25 @@ void client_send_msg(in_port_t src_port, const char *dest_addr_str,
                      in_port_t dest_port, const char *msg);
 
 /*Utils*/
+/*Ask controller for path packet*/
 void ask_controller(int sockfd, const char *controller_addr_str, in_port_t controller_port,
                     const char *src_addr_str, in_port_t src_port,
                     const char *dest_addr_str, in_port_t dest_port, 
-                    byte *path_buffer, int *path_data_len);
+                    byte *path_buffer, int *path_seg_len);
+
+/*Pack*/
+/*set src addr/port, dest addr/port 12B*/
+void set_addr_header(const char *src_addr_str, in_port_t src_port,
+                     const char *dest_addr_str, in_port_t dest_port,
+                     byte *addr_header_seg);
+
+/*Data*/
+void set_data(const char *msg, byte *data_seg, int *data_seg_len);
+
+/*Path*/
+void set_path(const byte *path_data, byte *path_seg, int path_seg_len);
+
+/*Cut addr seg and send*/
+void cut_send(int sockfd, byte *packet, int packet_len);
 
 #endif
